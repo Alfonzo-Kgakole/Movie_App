ba383515613879c27db0f8008df96efbbe89d105
Alfonzo <thulanikgakole@gmail.com>
2025-02-11 13:48:51 +0200

    done setting up the project with the font styles, font size, the colors

---
+++ react-native.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    project: {
+        ios: {},
+        android: {}
+    },
+    asserts: ['./src/assets'],
+    iosAssets: ["./src/asset"],
+  androidAssets: ["./src/assets"]
+}
---
+++ src/screen/MovieDetailScreen.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+interface MovieDetailScreenProps {}
+
+const MovieDetailScreen = (props: MovieDetailScreenProps) => {
+  return (
+    <View style={styles.container}>
+      <Text>MovieDetailScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {}
+});
+
+export default MovieDetailScreen;
+
